0baa018d048b07ba82d749bd23b9e4bef15e60c1
Pablo Aravena <paravena74@gmail.com>
2018-05-28 23:16:24 -0400

    first commit

---
+++ fitness-tracker/src/app/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required]]
+    });
+  }
+
+  public get email() {
+    return this.loginForm.get('email');
+  }
+
+  public get password() {
+    return this.loginForm.get('password');
+  }
+
+  public login() {
+    console.log(this.loginForm);
+  }
+}
